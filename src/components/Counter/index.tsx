4f91b816b67d401316b2f63f2dd48d51bbd76cd2
Vitor França <franc.vitoor@gmail.com>
2022-01-20 17:46:32 -0300

    Add counter

---
+++ src/components/Counter/index.tsx
@@ -0,0 +1,38 @@
+import { Box } from "@mui/material";
+import { useCounter, CountdownData } from "../../hooks/useCounter";
+import { CounterControls } from "../CounterControls";
+import { DisplayCounter } from "../DisplayCounter";
+
+export type CounterProps = {
+  initial: CountdownData["initialTime"];
+};
+
+export function Counter({ initial }: CounterProps) {
+  const {
+    minutes,
+    seconds,
+    startCountdown,
+    hasFinished,
+    isActive,
+    resetCountdown,
+  } = useCounter(initial);
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100%",
+      }}
+      bgcolor="cyan"
+    >
+      <DisplayCounter minutes={minutes} seconds={seconds} />
+      <CounterControls
+        startCountdown={startCountdown}
+        resetCountdown={resetCountdown}
+      />
+    </Box>
+  );
+}
